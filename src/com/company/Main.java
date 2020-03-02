ed7debb3d05261ab4e46bec55247084de4ac81be
Xavier O <xavier.odonovan.2021@student.lhschools.org>
2020-03-02 13:49:40 -0500

    intellij commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("┌────────────────────────┐");
+        System.out.println("│ Outcasts and Cheap Gas │");
+        System.out.println("│   14210 E. Broad St,   │");
+        System.out.println("│  Pataskala, OH 43073   │");
+        System.out.println("│                        │");
+        System.out.println("│ DATE        TIME       │");
+        System.out.println("│ 01/14/2020  2:07 P.M.  │");
+        System.out.println("│ Invoice Number# 322024 │");
+        System.out.println("│ AUTH#                  │");
+        System.out.println("│ VISA                   │");
+        System.out.println("│ **************24       │");
+        System.out.println("│                        │");
+        System.out.println("│ PUMP    PRODUCT    $/G │");
+        System.out.println("│  07       REG    $2.61 │");
+        System.out.println("│                        │");
+        System.out.println("│ GALLONS     FUEL TOTAL │");
+        System.out.println("│  15.00        $39.15   │");
+        System.out.println("│       THANK YOU!       │");
+        System.out.println("│ OUTCASTS AND CHEAP GAS │");
+        System.out.println("└────────────────────────┘");
+    }
+}
